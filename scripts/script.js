86b0f805a8b24faf48055abcaf74c7f0934648e5
Nekesa Purity <nekesapurity20@gmail.com>
2025-01-24 15:29:22 +0300

    added an array for akan names

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//akanName Array
+const maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleName =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function calculateAkanName: (Event);
+Event. preventDefault();
+
